b0b3788cbba6341156b5efbcb5ae2730d08c90b9
Rohit Taralekar <rohit.taralekar@aciworldwide.com>
2019-12-29 12:00:08 +0530

    Add project files.

---
+++ DatingApp1/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp1.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ValuesContext _context;
+
+        public ValuesController(ValuesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+    }
+}
---
+++ DatingApp1/Data/ValuesContext.cs
@@ -0,0 +1,11 @@
+﻿using DatingApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp1.Data
+{
+    public class ValuesContext: DbContext
+    {
+        public ValuesContext(DbContextOptions<ValuesContext> options): base(options) {}
+        public DbSet<Value> Values { get; set; } 
+    }
+}
